c0b097023d384451ad54ba7be0462e271cbfabc6
Moritz Marquardt <st166545@stud.uni-stuttgart.de>
2023-03-21 15:41:13 +0100

    working workflow with simple ca

--- workflow.py
+++ workflow.py
@@ -1,99 +1,56 @@
 import darsia as da
 import numpy as np
 import json
+import time
 
-config: dict = {}
+#1. step: make config file for corrections and preprocessing
+#use correction.py
+print("hi")
 
-baseline = da.Image(
-    "./data/test/DSC28345_t0.JPG", 
-    width=0.92, 
-    height=0.555
-    )
-# baseline.show()
-
-#Preprocessing of the images:
-roi_color_checker = [
-        [6717, 57],
-        [6722, 537],
-        [7447, 536],
-        [7438, 54],
-    ]
-
-#COLOR CORECTION
-color_correction = da.ColorCorrection(
-    roi=roi_color_checker, 
-    verbosity=False
-    )
-config["color"] = color_correction.config
-
-#DRIFT CORRECTION
-drift_correction = da.DriftCorrection(
-    base=baseline,
-    roi=roi_color_checker
-    )
-config["drift"] = drift_correction.config
-
-#CURVATURE CORRECTION
-curv_correction = da.CurvatureCorrection(
-    image = baseline.imgpath,
-    width = baseline.width,
-    height = baseline.height
-    )
-
-curv_correction.pre_bulge_correction(horizontal_bulge = 5e-9)
-# curv_correction.show_image()
-
-curv_correction.crop([
-        [261, 57],
-        [240, 4438],
-        [7625, 4439],
-        [7585, 57],
-    ])
-
-# print(curv_correction.config)
-# curv_correction.show_image()
-
-curv_correction.bulge_corection(
-    left = 78, 
-    right = 73, 
-    top = 0, 
-    bottom = 0
-    )
-# curv_correction.show_image()
-
-config["curvature"] = curv_correction.config
-# config_path = "./data/test/config.json"
-# with open(config_path, "w") as outfile:
-#     json.dump(config, outfile, indent=4)
+#open config from file
+with open('./data/test/config.json') as json_file:
+    config = json.load(json_file)
+curv_correction = da.CurvatureCorrection(config = config["curvature"])
+color_correction = da.ColorCorrection(config = config["color"])
 
 
 #Apply on another image with same camera setup with no drift correction, whats that?
 co2_image = da.Image(
-    "./data/test/DSC28537.JPG",
+    "./data/test/tracer_original.jpg",
     color_correction=color_correction,
     curvature_correction=curv_correction
 )
 # co2_image.show()
 
 baseline_co2 = da.Image(
-    "./data/test/DSC28345_t0.JPG",
+    "./data/test/tracer_t0_original.jpg",
     color_correction=color_correction,
     curvature_correction=curv_correction
 )
 # baseline_co2.show()
 
+'''reduction = da.MonochromaticReduction(color="red")
+reduced = reduction(co2_image.img)
+
+reduced_d = da.Image(
+    reduced, 
+    width=0.92, 
+    height=0.555,
+    color_space = "RGB"
+)
+reduced_d.show()'''
 
 #CONCENTRATION ANALYSIS
 # Construct concentration analysis for detecting the co2 concentration
 co2_analysis = da.ConcentrationAnalysis(
     baseline_co2,  # baseline image
-    da.MonochromaticReduction(color="gray"),  # signal reduction
+    da.MonochromaticReduction(color="red"),  # signal reduction
     None,  # signal balancing
     da.TVD(),  # restoration
     da.CombinedModel(  # signal to data conversion
         [
             da.LinearModel(scaling=4.0),
-            da.ClipModel(**{"min value": 0.0, "max value": 1.0}),
+            da.ClipModel(**{"min value": 0.0, "max value": 4.0}),
         ]
     ),
 )
@@ -101,3 +58,5 @@ co2_analysis = da.ConcentrationAnalysis(
 # Determine co2
 co2 = co2_analysis(co2_image)
 co2.plt_show()
+
+#tracer analysis is analogue
